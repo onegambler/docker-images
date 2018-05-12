c58f002370535638713547cd63b388624fcb0fe4
Roberto Magale <roberto.magale@skyscanner.net>
2018-05-12 09:33:12 +0100

    changed config location

--- pre-docker.sh
+++ pre-docker.sh
@@ -1,11 +1,10 @@
-sudo -i
-mkdir -p /mnt/extHD/docker/containers/{muximux,observium,portainer,radarr,sonarr,jackett}/config
+mkdir -p /mnt/extHD/raspberrypi/configs/{muximux,observium,portainer,radarr,sonarr,jackett}/config
 chown -R pi:pi /mnt/extHD/docker
-mkdir -p /mnt/extHD/docker/containers/observium/{config,logs,rrd}
+mkdir -p /mnt/extHD/raspberrypi/configs/observium/{config,logs,rrd}
 chmod -R 777 observium
 
 ### Installing nfs
-apt-get install nfs-kernel-server portmap nfs-common
+apt-get install nfs-kernel-server portmap nfs-common -y
 service nfs-server stop
 echo "/mnt/extHD           192.168.1.0/24(rw,nohide,insecure,no_subtree_check,async,all_squash)" >> /etc/exports
-service nfs-server starts 
+service nfs-server start
