2741746c90d0b287cc4005db79bb8753ec99d2d5
onegambler <roberto.magale@gmail.com>
2018-06-02 10:32:19 +0100

    added docker installation to pre-docker

--- pre-docker.sh
+++ pre-docker.sh
@@ -1,3 +1,14 @@
+### Install docker
+#curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" |     sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt install -y docker-ce python python-pip
+pip install docker-compose
+
 mkdir -p /mnt/extHD/raspberrypi/configs/{muximux,portainer,radarr,sonarr,jackett,mldonkey}
 chown -R pi:pi /mnt/extHD/raspberrypi
 
