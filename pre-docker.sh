64126cf9f3c73795cd6988a97bf1bcf2a1bc5cbc
onegambler <roberto.magale@gmail.com>
2018-06-07 22:22:27 +0100

    fixed a few things

--- pre-docker.sh
+++ pre-docker.sh
@@ -6,8 +6,12 @@ sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-pro
 curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
 echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
      $(lsb_release -cs) stable" |     sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update && sudo apt-get upgrade
 sudo apt install -y docker-ce python python-pip
 pip install docker-compose
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 mkdir -p /mnt/extHD/raspberrypi/configs/{muximux,portainer,radarr,sonarr,jackett,mldonkey}
 chown -R pi:pi /mnt/extHD/raspberrypi
